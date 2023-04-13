be222fe345951c484688d7bc6a13e23b602a2e28
bahdotsh <appu.yess@gmail.com>
2023-04-14 00:18:11 +0530

    v1.0.0

--- Formula/zp.rb
+++ Formula/zp.rb
@@ -2,11 +2,11 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Zp < Formula
-  desc "Copy the contents of the source file to the clipboard, allowing users to easily paste the contents into another file or program."
+  desc "Copy the contents of the source file or the std buffer to the clipboard, allowing users to easily paste the contents into another file or program."
   homepage "https://github.com/bahdotsh/zp"
-  url "https://github.com/bahdotsh/zp/releases/download/v0.2.0/zp-mac.tar.gz"
-  sha256 "1132d312e47ac30ca9478be2adec205f3b1d1acb665179b72afe0940c3819327"
-  version "0.2.0"
+  url "https://github.com/bahdotsh/zp/releases/download/v1.0.0/zp-mac.tar.gz"
+  sha256 "2cced886f151d83b089367089e6b5e2cee16aaec6aee1df13327620e21fbbfd0"
+  version "1.0.0"
 
   def install
     bin.install "zp"
